1264b61646e95b08ba22e04efd1c2b159f76d52b
thanh vo.u-paris <72513778+thanhvo-uparis@users.noreply.github.com>
2021-12-04 00:54:00 +0100

    add route Home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Home from './components/home/Home';
+import DogsPage from './components/dogsPage/DogsPage';
+import Cart from './components/cart/Cart';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  return(
+    
+    <Router>
+      <div>
+        <Routes>
+            <Route path="/" elemment={<Home/>} />
+        </Routes>
+      </div>
+    </Router>
+    
+  /*
+   <div>
+     <p>Bonsoir à tous</p>
+   </div>
+   */   
+  )
 }
 
 export default App;
---
+++ frontend/src/components/dogsPage/DogsPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DogsPage = () => {
+    return(
+        <div>
+            <p>Ici présente tous les chiens à vent</p>
+        </div>
+    )
+}
+
+export default DogsPage;
