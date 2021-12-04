ba6ccb15afeffe04c2bf159194cda479261c42ab
thanh vo.u-paris <72513778+thanhvo-uparis@users.noreply.github.com>
2021-12-04 20:17:52 +0100

    get data by axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import css from './App.css';
+import axios from 'axios';
 
 import NavBar from "./navBar/NavBar";
 
 const App = () => {
+  useEffect(() => {
+      function getData() {
+        const response = axios.get("v1/dogs");
+        return response;
+      }
+
+      getData().then((response) => console.log(response.data))
+  } , [])
+
   return(
     <div>
       <NavBar/>
